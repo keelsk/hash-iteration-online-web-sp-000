2772c7360d35fb86e18d0da7761861e8060e38db
Kirsten Keels <keelsk@gmail.com>
2020-02-25 02:42:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,6 +11,10 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(birtday_kids)
-  
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    if name[age] < 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
+  end
 end
